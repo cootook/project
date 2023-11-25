9d956b5b6b92fbc7b1487f038544e0b16703e5b3
Dmitrii Kutyrshin <cootook@gmail.com>
2023-11-25 06:32:18 -0500

    feat: add decorate fun login_required - helpers.py

---
+++ studio_app/helpers.py
@@ -0,0 +1,25 @@
+from flask import redirect, render_template, session
+from functools import wraps
+
+def log_user_in(username, password):
+    # Query database for username
+    #rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+
+    # Ensure username exists and password is correct
+    #if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+        #return apology("invalid username and/or password", 403)
+
+    # Remember which user has logged in
+    session["user_id"] = 1 #rows[0]["id"]
+
+    # Redirect user to home page
+    return redirect("/")
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/signin")
+        return f(*args, **kwargs)
+
+    return decorated_function
