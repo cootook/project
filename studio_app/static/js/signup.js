ea3b1fdde92f1b586fa67b1bb9c0b169b41b6a7b
Dmitrii Kutyrshin <cootook@gmail.com>
2023-11-30 07:29:57 -0500

    feat: implement password validation

--- studio_app/static/js/signup.js
+++ studio_app/static/js/signup.js
@@ -1,22 +1,32 @@
 window.addEventListener("load", () => {
+    
+    const ok = document.createElement("b", "status_pic");
+    ok.innerHTML = "&#10004;"
+    const not_ok = document.createElement("b", "status_pic");
+    not_ok.innerHTML = "&#10006;"
+
     const myInput = document.getElementById("password");
     const confirmation = document.getElementById("confirmation");
     const letter = document.getElementById("letter");
     const capital = document.getElementById("capital");
     const number = document.getElementById("number");
     const length = document.getElementById("length");
-    const submit = document.getElementById("signup_submit")
+    const submit = document.getElementById("form_signup");
+    const btn = document.getElementById("signup_submit");
 
-    const letter_before = document.getElementById("letter_before");
-    const capital_before = document.getElementById("capital_before");
-    const number_before = document.getElementById("number_before");
-    const length_before = document.getElementById("length_before");
+    letter.prepend(document.createElement("b", "status_pic"));
+    letter.status_pic.innerHTML = "&#10006;"
+    capital.prepend(not_ok);
+    number.prepend(not_ok);
+    length.prepend(not_ok);
+    // pass_same.prepend(not_ok);
 
     var is_letter = false;
     var is_capital = false;
     var is_number = false;
     var is_length = false;
     var is_same = false;
+
 
     // When the user clicks on the password field, show the message box
     // myInput.onfocus = function() {
@@ -28,64 +38,101 @@ window.addEventListener("load", () => {
     // document.getElementById("message").style.display = "none";
     // }
 
-    //prevent submit if password and confirmation are not the same
-    
-    submit.addEventListener("submit", () => {
-        if (true) {
-            return;
+    //validate password and confirmation are not the same
+
+    function set_valid_invalid (element, is_valid, to_disable) {
+        if (is_valid == true) {
+            to_disable.forEach(el => {
+                el.disabled = false;
+            })
+            element.classList.remove("invalid");
+            element.classList.add("valid");
+            element.status_pic.remove();
+            element.prepend(ok);
+        } else {
+            to_disable.forEach(el => {
+                el.disabled = true;
+            })
+            element.classList.remove("valid");
+            element.classList.add("invalid");
+            element.status_pic.remove();
+            element.prepend(not_ok);
+
         }
+        
+
+    }
+    submit.addEventListener("submit", (event) => {
+        if (myInput.value != confirmation.value) {
+            event.preventDefault();
+        };
     })
 
     // When the user starts to type something inside the password field
     myInput.onkeyup = function() {
 
+    // Validate the same
+    if(myInput.value != confirmation.value) {
+
+    } else {
+
+    }
+
     // Validate lowercase letters
     var lowerCaseLetters = /[a-z]/g;
-    if(myInput.value.match(lowerCaseLetters)) {
-        letter.classList.remove("invalid");
-        letter.classList.add("valid");
-        letter_before.innerHTML = "&#10004;";
+    is_letter = myInput.value.match(lowerCaseLetters);
+    if(is_letter) {
+        set_valid_invalid(letter, is_letter, [btn]);
+        letter_before.innerHTML = ok;
         
     } else {
-        letter.classList.remove("valid");
-        letter.classList.add("invalid");
-        letter_before.innerHTML = "&#10006;";
+        set_valid_invalid(letter, false, [btn]);
+        letter_before.innerHTML = not_ok;
         
     }
 
     // Validate capital letters
     var upperCaseLetters = /[A-Z]/g;
     if(myInput.value.match(upperCaseLetters)) {
+        btn.disabled = false;
         capital.classList.remove("invalid");
         capital.classList.add("valid");
-        capital_before.innerHTML = "&#10004;";
+        capital_before.innerHTML = ok;
+        is_capital = true;
     } else {
         capital.classList.remove("valid");
+        btn.disabled = true;
         capital.classList.add("invalid");
-        capital_before.innerHTML = "&#10006;";
+        capital_before.innerHTML = not_ok;
     }
 
     // Validate numbers
     var numbers = /[0-9]/g;
     if(myInput.value.match(numbers)) {
+        btn.disabled = false;
         number.classList.remove("invalid");
         number.classList.add("valid");
-        number_before.innerHTML = "&#10004;";
+        number_before.innerHTML = ok;
+        is_number = true;
     } else {
+        btn.disabled = true;
         number.classList.remove("valid");
         number.classList.add("invalid");
-        number_before.innerHTML = "&#10006;";
+        number_before.innerHTML = not_ok;
     }
 
     // Validate length
     if(myInput.value.length >= 8) {
+        btn.disabled = false;
         length.classList.remove("invalid");
         length.classList.add("valid");
-        length_before.innerHTML = "&#10004;";
+        length_before.innerHTML = ok;
+        is_length = true;
     } else {
+        btn.disabled = true;
         length.classList.remove("valid");
         length.classList.add("invalid");
-        length_before.innerHTML = "&#10006;";
+        length_before.innerHTML = not_ok;
     }
     }
 })
