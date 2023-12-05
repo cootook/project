2b1c5f6f0ada940b02b6bcac0c35787669b3712d
Dmitrii Kutyrshin <cootook@gmail.com>
2023-12-05 06:16:27 -0500

    feat: add nav bar items for admin

--- studio_app/webapp.py
+++ studio_app/webapp.py
@@ -29,6 +29,7 @@ app.register_error_handler(404, page_not_found)
 # Define lists of navbar items to be used in templates
 navbar_items = ["Appointments", "Account", "Pricing", "Articles", "Contact", "About", "LogOut"]
 navbar_items_not_loged_in = ["Pricing", "Articles", "Contact", "About", "SignIn", "SignUp"]
+navbar_items_admin = ["Appointments", "Account", "Clients", "Windows" "Pricing", "Articles", "Contact", "About", "LogOut"]
 
 @app.context_processor
 def inject_navbar_items():
