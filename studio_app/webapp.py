dceaa705a3e210a1b5d4f17d03d41ecf9939be29
Dmitrii Kutyrshin <cootook@gmail.com>
2023-12-19 06:42:13 -0500

    feat: add db query for account page

--- studio_app/webapp.py
+++ studio_app/webapp.py
@@ -72,10 +72,23 @@ def home():
 def about():    
     return render_template("about.html")
 
-@app.route("/account/")
+@app.route("/account/", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template("account.html")
+    try:
+        con = sqlite3.connect("./db.db")
+        cur = con.cursor()
+        # users (id INTEGER PRIMARY KEY, is_admin INT, is_clerck INT ,  name TEXT, email TEXT, lang TEXT, instagram TEXT, tel TEXT, is_subscribed_promo INT, avatar TEXT);
+        user = list(cur.execute("SELECT name, email, lang, instagram, tel, is_subscribed_promo, avatar FROM users WHERE id=?", (session.get("user_id"), )).fetchone())
+        for index in range(len(user)):
+            user[index] = "-" if user[index] == None else user[index]
+
+    except Exception as er:
+        con.close()
+        print("##/account/ --db connection")
+        print(er)
+        return  render_template("apology.html", error_message="Something went wrong")
+    return render_template("account.html", user=user)
 
 @app.route("/apology/")
 def apology():
