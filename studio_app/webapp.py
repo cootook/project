af7d1830a08de1cbebb0c1f5ec6d9a583c9b961f
Dmitrii Kutyrshin <cootook@gmail.com>
2023-12-25 06:55:01 -0500

    fix: delete commented code

--- studio_app/webapp.py
+++ studio_app/webapp.py
@@ -77,8 +77,6 @@ def about():
 def _account():
     return account.account()
 
-
-
 @app.route("/apology/")
 def apology():
     return render_template("apology.html")
@@ -87,115 +85,12 @@ def apology():
 @login_required
 def _appointments():
     return appointments.appointments()
-# def appointments():
-#     today = datetime.datetime.now()
-#     try:
-#         con = sqlite3.connect("./db.db") 
-#         cur = con.cursor()
-#         user_appoint_db = cur.execute("SELECT id, manicure, pedicure, message, slot_id, is_seen, is_aproved, is_canceled, amount_time_min FROM appointments WHERE user_id=? AND slot_id IN (SELECT slot_id FROM calendar WHERE (year=? AND month=? AND day>=?) OR (year=? AND month>?) or (year>?));", (session.get("user_id"), today.year, today.month, today.day, today.year, today.month, today.year)).fetchall()
-#         #user_appoint_db = user_appoint_db + cur.execute("SELECT id, service_name, slot_id, is_seen, is_aproved, is_canceled, amount_time_min FROM appointments WHERE user_id=? AND slot_id IN (SELECT slot_id FROM calendar WHERE year=? AND month>?);", (session.get("user_id"), today.year, today.month)).fetchall()
-#         #user_appoint_db = user_appoint_db + cur.execute("SELECT id, service_name, slot_id, is_seen, is_aproved, is_canceled, amount_time_min FROM appointments WHERE user_id=? AND slot_id IN (SELECT slot_id FROM calendar WHERE year>?);", (session.get("user_id"), today.year,)).fetchall()
-#         user_appoint = []
-#         for appointment in user_appoint_db:
-#             if appointment[7] == 0:
-#                 slot_db = list(cur.execute("SELECT year, month, day, hour, minute FROM calendar WHERE slot_id=?", (appointment[4],)).fetchone())
-#                 appointment_as_list = []
-#                 for el in appointment:
-#                     el = '-' if el == None else el
-#                     appointment_as_list.append(el)
-#                 appointment_as_list = appointment_as_list + slot_db
-#                 temp = []
-#                 print(appointment)
-#                 temp.append(appointment_as_list[0])
-#                 temp.append(get_service_name(appointment_as_list[1], appointment_as_list[2]))
-#                 temp = temp + appointment_as_list[4:14]
-#                 temp.append(appointment_as_list[3])
-#                 print(temp)
-#                 user_appoint.append(temp)
-#         user_appoint_sorted = sorted(user_appoint, key = lambda x: (x[7], x[8], x[9], x[10], x[11]))
-#     except Exception as er:
-#         con.close()
-#         print("##/appointments/ --db connection")
-#         print(er)
-#         return  render_template("apology.html", error_message="Something went wrong")
-       
-
-#     if request.method == "POST":
-#         try:
-#             appointment_id_to_cancel = int(request.form.get("appointment_id"))
-#             print("canceled appointment, id:")
-#             print(appointment_id_to_cancel)
-
-#             cur.execute("UPDATE appointments SET is_canceled=1 WHERE id=?", (appointment_id_to_cancel,))
-#             con.commit()
-#             con.close()
-#             return redirect("/appointments/")
-
-#         except Exception as er:
-#             con.close()
-#             print("##/appointments/ --cancell")
-#             print(er)
-#             return  render_template("apology.html", error_message="Something went wrong")
-
-#     return render_template("appointments.html", user_appoint=user_appoint_sorted)
-
 
 @app.route("/all_appointments/", methods=["GET", "POST"])
 @login_required
 @admin_only
 def _all_appointments():
     return all_appointments.all_appointments()
-# def all_appointments():
-#     today = datetime.datetime.now()
-#     try:
-#         con = sqlite3.connect("./db.db") 
-#         cur = con.cursor()
-#         user_appoint_db = cur.execute("SELECT user_id, id, manicure, pedicure, message, slot_id, is_seen, is_aproved, is_canceled, amount_time_min FROM appointments WHERE slot_id IN (SELECT slot_id FROM calendar WHERE year=? AND month=? AND day>=?) OR slot_id IN (SELECT slot_id FROM calendar WHERE year=? AND month>?) OR slot_id IN (SELECT slot_id FROM calendar WHERE year>?);", (today.year, today.month, today.day, today.year, today.month, today.year)).fetchall()
-#         user_appoint = []
-#         for appointment in user_appoint_db:
-#             if appointment[8] == 0: # check if was canceled
-#                 user_name = cur.execute("SELECT instagram FROM users WHERE id=?", (appointment[0],)).fetchone()[0]
-#                 slot_db = list(cur.execute("SELECT year, month, day, hour, minute FROM calendar WHERE slot_id=?", (appointment[5],)).fetchone())
-#                 appointment_as_list = []
-#                 for el in appointment:
-#                     el = '-' if el == None else el
-#                     appointment_as_list.append(el)
-#                 temp = []
-#                 temp.append(user_name)
-#                 temp = temp + appointment_as_list[1:2]
-#                 servise_name = ""
-#                 if appointment_as_list[2] == 1 and appointment_as_list[3] == 1:
-#                     servise_name = "combo"
-#                 elif appointment_as_list[2] == 1 and not appointment_as_list[3] == 1:
-#                     servise_name = "manicure"
-#                 else:
-#                     servise_name = "pedicure"
-#                 temp.append(servise_name)
-#                 temp = temp + appointment_as_list[5:10]
-#                 temp = temp + slot_db
-#                 temp.append(appointment_as_list[4])
-#                 user_appoint.append(temp)
-#         user_appoint_sorted = sorted(user_appoint, key = lambda x: (x[8], x[9], x[10], x[11], x[12]))
-#     except Exception as er:
-#         con.close()
-#         print("##/all_appointments/ --db connection")
-#         print(er)
-#         return  render_template("apology.html", error_message="Something went wrong")
-       
-
-#     if request.method == "POST":
-#         try:
-#             # appointment_id_to_cancel = int(request.form.get("appointment_id"))
-#             return redirect("/all_appointments/")
-
-#         except Exception as er:
-#             con.close()
-#             print("##/all_appointments/ --edit")
-#             print(er)
-#             return  render_template("apology.html", error_message="Something went wrong")
-
-#     return render_template("all_appointments.html", user_appoint=user_appoint_sorted)
-
 
 @app.route("/articles/")
 def articles():
