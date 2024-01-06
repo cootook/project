b01c0f5c00c7bf81c84743e60ef18a706ebf18cb
Dmitrii Kutyrshin <cootook@gmail.com>
2024-01-06 07:38:51 -0500

    feat: start email sending

--- studio_app/webapp.py
+++ studio_app/webapp.py
@@ -26,11 +26,10 @@ app.config['SESSION_FILE_THRESHOLD'] = 250
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=90)
 
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_APP_KEY')
 mail = Mail(app)
 
 Session(app)
