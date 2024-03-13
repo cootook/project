9c16f0115413d0feeeaf28c6bf1098b6dd0ed254
Dmitrii Kutyrshin <cootook@gmail.com>
2024-03-13 07:17:52 -0400

    feat: working on classes (database)

--- studio_app/db_classes.py
+++ studio_app/db_classes.py
@@ -1,38 +1,48 @@
 import datetime
 
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing import List, Optional
 
+
 class Base(DeclarativeBase):
     pass
 
-class Appointment(Base):
+db_base = SQLAlchemy(model_class=Base)
+
+class Appointment(db_base.Model):
     __tablename__ = "appointment"
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id = mapped_column(ForeignKey("user.id"))
-    user: Mapped["User"] = relationship(back_populates="appointment")
+    user: Mapped["User"] = relationship(back_populates="appointment", foreign_keys=user_id)
     service_id = mapped_column(ForeignKey("service.id"))
+    service = relationship("Service", foreign_keys=[service_id])
     at: Mapped[datetime.datetime]
     price: Mapped[Optional[float]]
     deposit_needed: Mapped[bool] = mapped_column(default=False)
     deposit: Mapped[Optional[float]]
     slot_id = mapped_column(ForeignKey("slot.id"))
+    slot = relationship("Slot", foreign_keys=[slot_id])
     amount_time_min: Mapped[int] = mapped_column(default = 90)
     done: Mapped[bool] = mapped_column(default = False)
-    done_by = mapped_column(ForeignKey("user.id")) 
+    done_by_id = mapped_column(ForeignKey("user.id")) 
+    done_by = relationship("User", foreign_keys=[done_by_id])
     done_at: Mapped[Optional[datetime.datetime]]
     approved: Mapped[bool] = mapped_column(default = False)
-    approved_by = mapped_column(ForeignKey("user.id"))
+    approved_by_id = mapped_column(ForeignKey("user.id"))
+    approved_by = relationship("User", foreign_keys=[approved_by_id])
     approved_at: Mapped[Optional[datetime.datetime]] 
     canceled: Mapped[bool] = mapped_column(default = False)
-    canceled_by = mapped_column(ForeignKey("user.id"))
+    canceled_by_id = mapped_column(ForeignKey("user.id"))
+    canceled_by = relationship("User", foreign_keys=[canceled_by_id])
     canceled_at: Mapped[Optional[datetime.datetime]]
     lust_update_at: Mapped[Optional[datetime.datetime]]
-    lust_update_by = mapped_column(ForeignKey("user.id"))
+    lust_update_by_id = mapped_column(ForeignKey("user.id"))
+    lust_update_by = relationship("User", foreign_keys=[lust_update_by_id])
     description: Mapped[str] = mapped_column(default = "")
 
-class Booking_message:
+class Booking_message(db_base.Model):
     __tablename__ = "booking_message"
     id: Mapped[int] = mapped_column(primary_key=True)
     appoint_id = mapped_column(ForeignKey("appointment.id"))
@@ -41,13 +51,13 @@ class Booking_message:
     edited_at: Mapped[Optional[datetime.datetime]]
     deleted: Mapped[bool] = mapped_column(default = False)
 
-class Language:
+class Language(db_base.Model):
     __tablename__ = "language"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Mf_recovery_code:
+class Mf_recovery_code(db_base.Model):
     __tablename__ = "mf_recovery_code"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
@@ -55,13 +65,13 @@ class Mf_recovery_code:
     user_id = mapped_column(ForeignKey("user.id"))
     user: Mapped["User"] = relationship(back_populates = "mf_recovery_codes")
 
-class Notification_type:
+class Notification_type(db_base.Model):
     __tablename__ = "notification_type"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Payment:
+class Payment(db_base.Model):
     __tablename__ = "payment"
     id: Mapped[int] = mapped_column(primary_key=True)
     method_id = mapped_column(ForeignKey("payment_method.id"))
@@ -76,43 +86,43 @@ class Payment:
     lust_update_by: Mapped[Optional[int]]
     description: Mapped[str] = mapped_column(default = "")
 
-class Payment_method:
+class Payment_method(db_base.Model):
     __tablename__ = "payment_method"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Payment_status:
+class Payment_status(db_base.Model):
     __tablename__ = "payment_status"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Payment_type:
+class Payment_type(db_base.Model):
     __tablename__ = "payment_type"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Role:
+class Role(db_base.Model):
     __tablename__ = "role"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Service:
+class Service(db_base.Model):
     __tablename__ = "service"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique = True)
     description: Mapped[str] = mapped_column(default = "")
 
-class Service_role:
+class Service_role(db_base.Model):
     __tablename__ = "service_role"
     id: Mapped[int] = mapped_column(primary_key=True)
     service_id: Mapped[int]
     role_id: Mapped[int]
 
-class Slot:
+class Slot(db_base.Model):
     __tablename__ = "slot"
     id: Mapped[int] = mapped_column(primary_key=True)
     date_time: Mapped[datetime.datetime]
@@ -122,7 +132,7 @@ class Slot:
     occupied: Mapped[bool]
     occupied_by_appoint = mapped_column(ForeignKey("appointment.id"))
 
-class User:
+class User(db_base.Model):
     _tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique = True)
@@ -139,24 +149,28 @@ class User:
     name: Mapped[str]   
     instagram: Mapped[str] 
     tel: Mapped[Optional[str]]
-    language_id = mapped_column(ForeignKey("language.id")) 
+    language_id = mapped_column(ForeignKey("language.id"))
+    language = relationship("Language", foreign_keys=[language_id]) 
     internal_description: Mapped[str] = mapped_column(unique = True) 
     picture_path: Mapped[str] = mapped_column(unique = True) 
-    appointment: Mapped[List["Appointment"]] = relationship(back_populates = "user")
+    appointment_id: Mapped[List[int]] = re
+    appointment: Mapped[List["Appointment"]] = relationship(back_populates = "user", foreign_keys=[appointment_id])
     role: Mapped[List["User_role"]] = relationship(back_populates = "user")
     lust_update_at: Mapped[datetime.datetime] 
-    lust_update_by = mapped_column(ForeignKey("user.id"))
+    lust_update_by_id = mapped_column(ForeignKey("user.id"))
+    lust_update_by = relationship("User", foreign_keys=[lust_update_by_id])
     deleted: Mapped[bool] = mapped_column(default = False)
     deleted_at: Mapped[Optional[datetime.datetime]]
-    deleted_by = mapped_column(ForeignKey("user.id"))
+    deleted_by_id = mapped_column(ForeignKey("user.id"))
+    deleted_by = relationship("User", foreign_keys=[deleted_by_id])
 
-class User_notification:
+class User_notification(db_base.Model):
     __tablename__ = "user_notification"
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id = mapped_column(ForeignKey("user.id"))
-    type_id = mapped_column(ForeignKey("notification_type"))
+    type_id = mapped_column(ForeignKey("notification_type.id"))
 
-class User_role:
+class User_role(db_base.Model):
     __tablename__ = "user_role"
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id = mapped_column(ForeignKey("user.id"))
--- studio_app/webapp.py
+++ studio_app/webapp.py
@@ -3,7 +3,6 @@ import re
 import secrets
 import sqlite3
 import datetime
-import studio_app.db_classes as db_class
 import smtplib, ssl
 
 # from db_classes import Role
@@ -15,6 +14,8 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import create_engine
+from studio_app.db_classes import db_base
+from studio_app.db_classes import Appointment, Booking_message, Language, Mf_recovery_code, Notification_type, Payment, Payment_method, Payment_status, Payment_type, Role, Service, Service_role, Slot, User, User_notification, User_role
 from studio_app.helpers import log_user_in, log_user_out, login_required, validate_password, page_not_found, does_user_exist, not_loged_only, admin_only, get_service_name
 from .rout_handlers import *
 
@@ -24,6 +25,12 @@ from .rout_handlers import *
 
 # from sqlalchemy import Integer, String, ForeignKey
 # from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+# Flask-SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import List, Optional
 
 app = Flask(
                 __name__,
@@ -48,9 +55,14 @@ app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_APP_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 # mail = Mail(app)
 
+# flask-sqlalchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
 Session(app)
 
-engine = create_engine(os.environ.get('DB_PATH'), echo=True)
+db_base.init_app(app)
+
+# engine = create_engine(os.environ.get('DB_PATH'), echo=True)
 
 app.register_error_handler(404, page_not_found)
 
@@ -72,171 +84,11 @@ def inject_navbar_items_not_loged_in():
 def inject_navbar_items_admin():
     return dict(navbar_items_admin=navbar_items_admin)
 
-# classes
-# class Base(DeclarativeBase):
-#     pass
+with app.app_context():
+    db_base.create_all()
 
-# class Appointment(Base):
-#     __tablename__ = "appointment"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     user_id = mapped_column(ForeignKey("user.id"))
-#     user: Mapped["User"] = relationship(back_populates="appointment")
-#     service_id = mapped_column(ForeignKey("service.id"))
-#     at: Mapped[datetime.datetime]
-#     price: Mapped[Optional[float]]
-#     deposit_needed: Mapped[bool] = mapped_column(default=False)
-#     deposit: Mapped[Optional[float]]
-#     slot_id = mapped_column(ForeignKey("slot.id"))
-#     amount_time_min: Mapped[int] = mapped_column(default = 90)
-#     done: Mapped[bool] = mapped_column(default = False)
-#     done_by = mapped_column(ForeignKey("user.id")) 
-#     done_at: Mapped[Optional[datetime.datetime]]
-#     approved: Mapped[bool] = mapped_column(default = False)
-#     approved_by = mapped_column(ForeignKey("user.id"))
-#     approved_at: Mapped[Optional[datetime.datetime]] 
-#     canceled: Mapped[bool] = mapped_column(default = False)
-#     canceled_by = mapped_column(ForeignKey("user.id"))
-#     canceled_at: Mapped[Optional[datetime.datetime]]
-#     lust_update_at: Mapped[Optional[datetime.datetime]]
-#     lust_update_by = mapped_column(ForeignKey("user.id"))
-#     description: Mapped[str] = mapped_column(default = "")
+new_user = User()
 
-# class Booking_message:
-#     __tablename__ = "booking_message"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     appoint_id = mapped_column(ForeignKey("appointment.id"))
-#     author_id = mapped_column(ForeignKey("user.id"))
-#     at: Mapped[datetime.datetime]
-#     edited_at: Mapped[Optional[datetime.datetime]]
-#     deleted: Mapped[bool] = mapped_column(default = False)
-
-# class Language:
-#     __tablename__ = "language"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Mf_recovery_code:
-#     __tablename__ = "mf_recovery_code"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-#     user_id = mapped_column(ForeignKey("user.id"))
-#     user: Mapped["User"] = relationship(back_populates = "mf_recovery_codes")
-
-# class Notification_type:
-#     __tablename__ = "notification_type"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Payment:
-#     __tablename__ = "payment"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     method_id = mapped_column(ForeignKey("payment_method.id"))
-#     type_id = mapped_column(ForeignKey("payment_type.id"))
-#     amount: Mapped[float]
-#     payed: Mapped[bool] = mapped_column(default = False)
-#     status_id: Mapped[int]
-#     accepted_by: Mapped[int]
-#     payed_by: Mapped[int]
-#     at: Mapped[datetime.datetime]
-#     lust_update_at: Mapped[Optional[datetime.datetime]]
-#     lust_update_by: Mapped[Optional[int]]
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Payment_method:
-#     __tablename__ = "payment_method"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Payment_status:
-#     __tablename__ = "payment_status"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Payment_type:
-#     __tablename__ = "payment_type"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Role:
-#     __tablename__ = "role"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Service:
-#     __tablename__ = "service"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     name: Mapped[str] = mapped_column(unique = True)
-#     description: Mapped[str] = mapped_column(default = "")
-
-# class Service_role:
-#     __tablename__ = "service_role"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     service_id: Mapped[int]
-#     role_id: Mapped[int]
-
-# class Slot:
-#     __tablename__ = "slot"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     date_time: Mapped[datetime.datetime]
-#     opened: Mapped[bool]
-#     opened_by = mapped_column(ForeignKey("user.id"))
-#     opened_at: Mapped[datetime.datetime]
-#     occupied: Mapped[bool]
-#     occupied_by_appoint = mapped_column(ForeignKey("appointment.id"))
-
-# class User:
-#     _tablename__ = "user"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     email: Mapped[str] = mapped_column(unique = True)
-#     password: Mapped[Optional[str]]
-#     active: Mapped[str] = mapped_column(default = True)
-#     fs_uniquifier: Mapped[str] = mapped_column(unique = True) 
-#     confirmed_at: Mapped[Optional[datetime.datetime]]
-#     last_login_at: Mapped[Optional[datetime.datetime]]
-#     current_login_at: Mapped[Optional[datetime.datetime]]
-#     last_login_ip: Mapped[str] 
-#     current_login_ip: Mapped[str] 
-#     login_count: Mapped[int] 
-#     mf_recovery_codes: Mapped[List["Mf_recovery_code"]] = relationship(back_populates = "user")
-#     name: Mapped[str]   
-#     instagram: Mapped[str] 
-#     tel: Mapped[Optional[str]]
-#     language_id = mapped_column(ForeignKey("language.id")) 
-#     internal_description: Mapped[str] = mapped_column(unique = True) 
-#     picture_path: Mapped[str] = mapped_column(unique = True) 
-#     appointment: Mapped[List["Appointment"]] = relationship(back_populates = "user")
-#     role: Mapped[List["User_role"]] = relationship(back_populates = "user")
-#     lust_update_at: Mapped[datetime.datetime] 
-#     lust_update_by = mapped_column(ForeignKey("user.id"))
-#     deleted: Mapped[bool] = mapped_column(default = False)
-#     deleted_at: Mapped[Optional[datetime.datetime]]
-#     deleted_by = mapped_column(ForeignKey("user.id"))
-
-# class User_notification:
-#     __tablename__ = "user_notification"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     user_id = mapped_column(ForeignKey("user.id"))
-#     type_id = mapped_column(ForeignKey("notification_type"))
-
-# class User_role:
-#     __tablename__ = "user_role"
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     user_id = mapped_column(ForeignKey("user.id"))
-#     user: Mapped["User"] = relationship(back_populates = "role")
-#     role_id = mapped_column(ForeignKey("role.id"))
-#     set_by = mapped_column(ForeignKey("user.id"))
-#     set_at: Mapped[datetime.datetime]
-
-new_role = db_class.Role()
-new_role.name = "test"
-print(new_role.name)
 
 @app.route("/test_mail_py/", methods=["GET", "POST"])
 @login_required
