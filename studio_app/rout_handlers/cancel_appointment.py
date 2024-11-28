8113062e4f3ac2e7a3f598c3a6186986dbc99d0a
cootook@gmail.com <cootook@gmail.com>
2024-11-28 08:24:05 -0500

    feat: implement cancel_appointment using db_v2

--- studio_app/rout_handlers/cancel_appointment.py
+++ studio_app/rout_handlers/cancel_appointment.py
@@ -1,39 +1,30 @@
-import sqlite3
+import datetime
 
-from flask import redirect, render_template, request, session
+from sqlalchemy import update, select
+from studio_app.db_classes import Appointment, db_base
+from flask import redirect, render_template, request
+from flask_security import current_user
+from flask import redirect, render_template, request
 
 def cancel_appointment():
-    # try:
-    #     user_id_cancel = int(request.form.get("user_id_cancel"))
-    #     booking_id_cancel = int(request.form.get("booking_id_cancel"))
-    #     cancel_message = request.form.get("cancel_message")
+    try:
+        user_id_cancel = int(request.form.get("user_id_cancel"))
+        booking_id_cancel = int(request.form.get("booking_id_cancel"))
+        cancel_message = request.form.get("cancel_message")
 
-    #     if session.get("is_admin") == 0 and user_id_cancel != session.get("user_id"):
-    #         print("##/cancel_appointment/ --Access denied. User # ", session.get("user_id"))
-    #         return  render_template("apology.html", error_message="Access denied.")
-    #     else:
-    #         print(f"#canceling for \nuser #{user_id_cancel} \nbooking id #{booking_id_cancel} \nwith msg: '{cancel_message}',\n by #{session.get("user_id")} is admin - {session.get("is_admin")}")
-        
-    # except Exception as er:
-    #     print("##/cancel_appointment/ --form request")
-    #     print(er)
-    #     return  render_template("apology.html", error_message="Something went wrong")
-    
-    # try:
-    #     con = sqlite3.connect("./db.db") 
-    #     cur = con.cursor()
-    #     cur.execute("UPDATE appointments SET is_canceled=1, message=? WHERE user_id=? AND slot_id=?", (cancel_message, user_id_cancel, booking_id_cancel))
-    #     con.commit()
-    #     con.close()
-    #     if session.get("is_admin") == 0:
-    #         return redirect("/appointments/")
-    #     else:
-    #         return redirect("/all_appointments/")
-        
-    # except Exception as er:
-    #     con.close()
-    #     print("##/cancel_appointment/ --db")
-    #     print(er)
-    #     return  render_template("apology.html", error_message="Something went wrong")
+    except Exception as er:
+        print("##/cancel_appointment/ --form request")
+        print(er)
+        return  render_template("apology.html", error_message="Something went wrong")
+    messages = db_base.session.scalar(select(Appointment.description).where(Appointment.id == booking_id_cancel, Appointment.user_id == user_id_cancel))
+    db_base.session.execute(update(Appointment).where(Appointment.id == booking_id_cancel, Appointment.user_id == user_id_cancel).values(
+        description = cancel_message + " | " + messages,
+        lust_update_at = datetime.datetime.now(),
+        lust_update_by_id = current_user.id,
+        canceled_at = datetime.datetime.now(),
+        canceled_by_id = current_user.id,
+        canceled = True
+        ))
+    db_base.session.commit()
     
     return redirect("/all_appointments/")
