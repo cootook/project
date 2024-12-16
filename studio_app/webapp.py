fc1f38573e10e3e1f6ed7b2af814c9c4a8f8d237
cootook@gmail.com <cootook@gmail.com>
2024-12-16 06:55:42 -0500

    feat: add confirmation code template

--- studio_app/webapp.py
+++ studio_app/webapp.py
@@ -97,6 +97,10 @@ def inject_navbar_items_not_loged_in():
 def inject_navbar_items_admin():
     return dict(navbar_items_admin=navbar_items_admin)
 
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    return render_template("booking_sms_confirmation_code.html", appointment_id=222, phone="+16465191763")
+
 @app.route("/test_mail_py/", methods=["GET", "POST"])
 @login_required
 def test_mail_py():
