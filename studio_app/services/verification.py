998000dae71bf20e7c9461631339aab5e986a040
cootook <cootook@gmail.com>
2025-01-24 13:48:31 -0500

    fix: revert wrong changes

--- studio_app/services/verification.py
+++ studio_app/services/verification.py
@@ -16,7 +16,7 @@ class SmsVerificationOfBooking:
             return False
     
     def send_code(self):
-        text = f"{Config.TWILIO_SmsService_HEADER}your code: {self.booking.confirmation_code} {Config.TWILIO_SmsService_FOOTER}"
+        text = f"{Config.TWILIO_SMS_HEADER}your code: {self.booking.confirmation_code} {Config.TWILIO_SMS_FOOTER}"
         new_sms = SmsService(self.booking.client_phone, text).send()
         return new_sms
     
