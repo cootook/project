3ab1c233059dffd0cb3b02c7cd9d79a68e4c5c3f
cootook@gmail.com <cootook@gmail.com>
2025-02-01 19:56:07 -0500

    refactor: seeder

--- studio_app/repositories/slot_repository.py
+++ studio_app/repositories/slot_repository.py
@@ -1,2 +1,8 @@
+import datetime
+
+from ..models.slot import SlotModel
+from ..models.base import db_base
+from ..config import Config
+
 class SlotRepository:
     pass
--- studio_app/seeders/seeder.py
+++ studio_app/seeders/seeder.py
@@ -4,20 +4,24 @@ from .test_user_seeder import seed_test_user
 
 
 class Seeder:
-    @staticmethod
-    def seed_all():
-        seed_roles()
-        seed_admin()
-        seed_test_user()
-
-    @staticmethod
-    def seed_admin():
-        seed_admin()
-
-    @staticmethod
-    def seed_test_user():
-        seed_test_user()
-
-    @staticmethod
-    def seed_roles():
-        seed_roles()
+    def __init__(self, seeders: dict=None):
+        self.seeders = seeders or {
+            'roles': seed_roles,
+            'admin': seed_admin,
+            'test_user': seed_test_user
+        }
+    
+    def add_seeder(self, name, seeder_func):
+        self.seeders[name] = seeder_func
+        
+    def seed(self, seeder_name=None):
+        if seeder_name:
+            if seeder_name not in self.seeders:
+                raise ValueError(f"Unknown seeder: {seeder_name}")
+            self.seeders[seeder_name]()
+        else:
+            self.seed_all()
+            
+    def seed_all(self):
+        for seeder in self.seeders.values():
+            seeder()
