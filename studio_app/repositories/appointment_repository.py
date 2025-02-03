d1c8f52a95ad67acabc5734cc904f1ccef560823
cootook <cootook@gmail.com>
2025-02-03 15:04:48 -0500

    refactor: move methods to appointment_repository

---
+++ studio_app/repositories/appointment_repository.py
@@ -0,0 +1,27 @@
+from sqlalchemy import select, update
+from ..models.base import db_base
+from ..models.appointment import AppointmentModel
+from random import randrange
+
+
+class AppointmentRepository:
+    def __init__(self, db_session=None):
+        self.db = db_session or db_base.session
+    
+    def create(self, **kwargs):
+        new_appointment = AppointmentModel(sms_confirmation_code=randrange(1000, 9999, 11), **kwargs)
+        self.db.session.add(new_appointment)
+        self.db.session.commit()
+        return new_appointment
+    
+    def get_by_id(self, id) -> AppointmentModel:
+        return self.db.session.scalar(select(AppointmentModel).where(AppointmentModel.id == id))
+
+    def set_get_confirmation_code(self, appointment: AppointmentModel) -> int:
+        sms_confirmation_code = randrange(1000, 9999, 11)
+        self.db.session.execute(update(AppointmentModel).where(AppointmentModel.id == appointment.id).values(sms_confirmation_code = sms_confirmation_code))
+        self.db.session.commit()
+        return sms_confirmation_code
+    
+    def set_phone_confirmed(self, appointment: AppointmentModel):
+        self.db.session.execute(update(AppointmentModel).where(AppointmentModel.id == appointment.id).values(phone_confirmed = True))
