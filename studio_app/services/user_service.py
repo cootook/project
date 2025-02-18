900970807a628768d47e502e39bb2afe1c61ec1c
cootook@gmail.com <cootook@gmail.com>
2025-02-18 06:42:48 -0500

    refactor: user_service

--- studio_app/services/user_service.py
+++ studio_app/services/user_service.py
@@ -1,13 +1,26 @@
-from ..db_classes import User
-from flask_security import current_user, hash_password, login_user, logout_user
-from ..config import Config
+from ..models.user import UserModel
+from ..repositories.user_repository import UserRepository
+from flask_security import login_user, logout_user
 
 class UserService:
-    def __init__(self):
-        pass
+    def __init__(self, user: UserModel=None):
+        self.user = user
+        self.user_repo = UserRepository()
+    
 
-    @staticmethod
-    def login_by_id(id):
-        user = User.get_user_by_id(id)
-        logout_user()
-        return login_user(user)
+    def login_by_id(self, id: int) -> bool:
+        if self.user is not None:
+            logout_user(self.user)
+
+        try:
+            self.user = self.user_repo.get_user_by_id(id)
+            return login_user(self.user)
+        except Exception as e:
+            self._log_error("Failed log in user by ID", e)
+            return False
+    
+    def _log_error(self, message: str, exception: Exception = None) -> None:
+        error_msg = f"Error: {message}"
+        if exception:
+            error_msg += f" - {str(exception)}"
+        print(error_msg)
