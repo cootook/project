e52028861d5c51333391f432a8339a369ee6e13c
cootook@gmail.com <cootook@gmail.com>
2025-02-23 15:26:34 -0500

    refactor: get rid of try-catch

--- studio_app/services/user_service.py
+++ studio_app/services/user_service.py
@@ -5,22 +5,12 @@ from flask_security import login_user, logout_user
 class UserService:
     def __init__(self, user: UserModel=None):
         self.user = user
-        self.user_repo = UserRepository()
-    
+        self.user_repo = UserRepository()    
 
     def login_by_id(self, id: int) -> bool:
         if self.user is not None:
             logout_user(self.user)
 
-        try:
-            self.user = self.user_repo.get_user_by_id(id)
-            return login_user(self.user)
-        except Exception as e:
-            self._log_error("Failed log in user by ID", e)
-            return False
+        self.user = self.user_repo.get_user_by_id(id)
+        return login_user(self.user)
     
-    def _log_error(self, message: str, exception: Exception = None) -> None:
-        error_msg = f"Error: {message}"
-        if exception:
-            error_msg += f" - {str(exception)}"
-        print(error_msg)
