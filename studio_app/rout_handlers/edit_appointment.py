968c06d65533330edcb2b2d818e6f216316e58aa
cootook <cootook@gmail.com>
2025-02-24 19:22:31 -0500

    refactor: in progress: edit_appointment

--- studio_app/rout_handlers/edit_appointment.py
+++ studio_app/rout_handlers/edit_appointment.py
@@ -6,38 +6,58 @@ from studio_app.db_classes import Appointment, Service, db_base
 from flask import redirect, render_template, request
 from flask_security import current_user
 
-def edit_appointment():
-    form = request.form
-    new_date = request.form.get("new_date")
-    new_date_py = datetime.datetime.strptime(new_date, '%Y-%m-%dT%H:%M')
-    new_duration = int(request.form.get("new_duration"))
-    new_message = request.form.get("new_message")
-    user_id_edit = int(request.form.get("user_id_edit"))
-    booking_id_edit = int(request.form.get("booking_id_edit"))
-    
-    services = db_base.session.scalars(select(Service.name).where(Service.deleted == False))
-    list_of_services = []
-    for s in services:
-        list_of_services.append(s)
+from flask import redirect, request
+from ..services.appointment_service import AppointmentService
+from flask import redirect, request, Response
+from typing import Tuple, Union
+from http import HTTPStatus
+from ..error_handlers.appointment_error_handler import AppointmentErrorHandler
+from ..services.service_service import ServiceService
+from ..repositories.service_repository import ServiceRepository
 
-    new_service = []
-    for s in list_of_services:
-        if s in form:
-            new_service.append(s)
-    appointment_to_edit = db_base.session.scalar(select(Appointment).where(Appointment.id == booking_id_edit, Appointment.user_id == user_id_edit))
+appointment_error_handler = AppointmentErrorHandler()
+service_for_services = ServiceService()
+service_repo = ServiceRepository()
 
-    if appointment_to_edit is None:
-        return render_template("apology.html", error_message="Sorry. Something went wrong. Please, try again later.")
-    else:
-        messages = db_base.session.scalar(select(Appointment.description).where(Appointment.id == booking_id_edit, Appointment.user_id == user_id_edit))
-        db_base.session.execute(update(Appointment).where(Appointment.id == booking_id_edit, Appointment.user_id == user_id_edit).values(
-            at = new_date_py,
-            amount_time_min = new_duration,
-            description = new_message + " | " + messages,
-            service = dumps(new_service),
-            last_update_at = datetime.datetime.now(),
-            last_update_by_id = current_user.id
-        ))
-        db_base.session.commit()
+
+def edit_appointment() -> Tuple[Union[Response, str], int]:
+    try:
+        data = _extract_new_data()
+        new_date_time_pythonic = datetime.datetime.strptime(
+            data['date_time'], 
+            '%Y-%m-%dT%H:%M'
+            )
+    # check if appointment exists
+    # get old description
+    # append old description to new message
+    # update appointment 
+        # messages = db_base.session.scalar(select(Appointment.description).where(Appointment.id == booking_id_edit, Appointment.user_id == user_id_edit))
+        # db_base.session.execute(update(Appointment).where(Appointment.id == booking_id_edit, Appointment.user_id == user_id_edit).values(
+        #     at = new_date_py,
+        #     amount_time_min = new_duration,
+        #     description = new_message + " | " + messages,
+        #     service = dumps(new_service),
+        #     last_update_at = datetime.datetime.now(),
+        #     last_update_by_id = current_user.id
+        # ))
+    except ValueError as e:
+        return appointment_error_handler.handle_appointment_error(
+            error=e,
+            appointment_id=data.get('booking_id'),
+            additional_data={'user_id': data.get('user_id')}
+        )
       
-    return redirect("/all_appointments/")
+    return redirect("/all_appointments/"), HTTPStatus.OK
+
+def _extract_new_data() -> dict:
+    try:
+        return {
+            'user_id': int(request.form.get("user_id_edit")),
+            'booking_id': int(request.form.get("booking_id_edit")),
+            'message': float(request.form.get("new_message", "")),
+            'duration': int(request.form.get("new_duration")),
+            'date_time': request.form.get("new_date"),
+            'service': service_for_services.get_list_of_services_from_form_data_dict(request.form)
+        }
+    except (TypeError, ValueError) as e:
+        raise ValueError(f"Failed to parse form data: {str(e)}")
